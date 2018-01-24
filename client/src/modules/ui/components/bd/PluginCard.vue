71ac54911ab1b2907322f165ab261ede6082bc73
Jiiks <jiiks@windowslive.com>
2018-01-24 08:42:10 +0200

    Styling

--- client/src/modules/ui/components/bd/PluginCard.vue
+++ client/src/modules/ui/components/bd/PluginCard.vue
@@ -4,7 +4,10 @@
     /*Imports*/
     import { Button, ButtonGroup } from '../generic';
     import MiSettings from 'vue-material-design-icons/settings.vue';
-    const components = { MiSettings, Button, ButtonGroup };
+    import MiReload from 'vue-material-design-icons/refresh.vue';
+    import MiEdit from 'vue-material-design-icons/pencil.vue';
+    import MiDelete from 'vue-material-design-icons/delete.vue';
+    const components = { MiSettings, Button, ButtonGroup, MiReload, MiEdit, MiDelete };
 
     export default {
         props: ['plugin', 'togglePlugin'],

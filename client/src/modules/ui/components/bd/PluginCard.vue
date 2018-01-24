b3218d636aac4210775c79f10d347c31f3930f8c
Jiiks <jiiks@windowslive.com>
2018-01-24 07:47:04 +0200

    Styling, common components and add buttons to plugin card

--- client/src/modules/ui/components/bd/PluginCard.vue
+++ client/src/modules/ui/components/bd/PluginCard.vue
@@ -1,8 +1,14 @@
 <template src="./templates/PluginCard.html"></template>
 
 <script>
+    /*Imports*/
+    import { Button, ButtonGroup } from '../generic';
+    import MiSettings from 'vue-material-design-icons/settings.vue';
+    const components = { MiSettings, Button, ButtonGroup };
+
     export default {
         props: ['plugin', 'togglePlugin'],
+        components,
         name: "PluginCard"
     }
 </script>
---
+++ client/src/modules/ui/components/generic/ButtonGroup.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bd-button-group">
+        <slot/>
+    </div>
+</template>
+<script>
+    export default {
+    }
+</script>

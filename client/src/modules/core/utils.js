63c239f36f1efc7ba3620632f042f426f103db50
samogot <samogot@gmail.com>
2018-01-27 16:40:35 +0200

    Extract global BetterDiscord.vendor into separate module, so it can be used through require

--- client/src/modules/core/utils.js
+++ client/src/modules/core/utils.js
@@ -5,10 +5,11 @@
  * https://github.com/JsSucks - https://betterdiscord.net
  *
  * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree. 
+ * LICENSE file in the root directory of this source tree.
 */
 
 const { Module } = require('./modulebase');
+const { Vendor } = require('./vendor');
 const fs = window.require('fs');
 const path = window.require('path');
 
@@ -29,7 +30,7 @@ class Logger {
         }
         level = this.parseLevel(level);
         console[level]('[%cBetter%cDiscord:%s] %s', 'color: #3E82E5', '', `${module}${level === 'debug' ? '|DBG' : ''}`, message);
-        logs.push(`[${BetterDiscord.vendor.moment().format('DD/MM/YY hh:mm:ss')}|${module}|${level}] ${message}`);
+        logs.push(`[${Vendor.moment().format('DD/MM/YY hh:mm:ss')}|${module}|${level}] ${message}`);
         window.bdlogs = logs;
     }
 
@@ -78,7 +79,7 @@ class Utils {
             });
         }
     }
-    
+
 }
 
 class FileUtils {
@@ -208,13 +209,13 @@ class Filters {
         };
     }
 
-    static byDisplayName(name) { 
+    static byDisplayName(name) {
         return module => {
             return module && module.displayName === name;
         };
     }
 
-    static combine(...filters) { 
+    static combine(...filters) {
         return module => {
             return filters.every(filter => filter(module));
         };

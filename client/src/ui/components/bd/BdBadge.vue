92634a2f982da2315764b2512e8f71cf84460dc8
Jiiks <jiiks@windowslive.com>
2018-01-31 16:10:35 +0200

    BD Dev/Contributor badge

---
+++ client/src/ui/components/bd/BdBadge.vue
@@ -0,0 +1,32 @@
+/**
+ * BetterDiscord BD Badge Component
+ * Copyright (c) 2015-present Jiiks/JsSucks - https://github.com/Jiiks / https://github.com/JsSucks
+ * All rights reserved.
+ * https://betterdiscord.net
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+*/
+
+<template>
+    <div class="bd-profile-badges-wrap">
+        <div class="bd-profile-badges">
+            <div v-if="developer" class="bd-profile-badge bd-profile-badge-developer"></div>
+            <div v-id="contributor" class="bd-profile-badge bd-profile-badge-contributor"></div>
+        </div>
+    </div>
+</template>
+<script>
+    // Imports
+    import { shell } from 'electron';
+
+    export default {
+        props: ['developer', 'contributor'],
+        methods: {
+            onClick() {
+                if (this.developer) return shell.openExternal('https://github.com/JsSucks/BetterDiscordApp');
+                if (this.contributor) return shell.openExternal('https://github.com/JsSucks/BetterDiscordApp/graphs/contributors');
+            }
+        }
+    }
+</script>

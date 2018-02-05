247274366091d4db1822a7134df4938ad1596f70
Jiiks <jiiks@windowslive.com>
2018-02-05 16:19:24 +0200

    Theme manager base

--- client/src/modules/thememanager.js
+++ client/src/modules/thememanager.js
@@ -9,6 +9,43 @@
 */
 
 import ContentManager from './contentmanager';
+import { DOM } from 'ui';
+import { FileUtils } from 'common';
+
+class Theme {
+
+    constructor(themeInternals) {
+        this.__themeInternals = themeInternals;
+        this.enable = this.enable.bind(this);
+        this.disable = this.disable.bind(this);
+    }
+
+    get configs() { return this.__themeInternals.configs }
+    get info() { return this.__themeInternals.info }
+    get icon() { return this.info.icon }
+    get paths() { return this.__themeInternals.paths }
+    get main() { return this.__themeInternals.main }
+    get defaultConfig() { return this.configs.defaultConfig }
+    get userConfig() { return this.configs.userConfig }
+    get name() { return this.info.name }
+    get authors() { return this.info.authors }
+    get version() { return this.info.version }
+    get themePath() { return this.paths.contentPath }
+    get dirName() { return this.paths.dirName }
+    get enabled() { return this.userConfig.enabled }
+    get themeConfig() { return this.userConfig.config }
+    get css() { return this.__themeInternals.css }
+    get id() { return this.name.toLowerCase().replace(/\s+/g, '-') }
+
+    enable() {
+        DOM.injectTheme(this.css, this.id);
+    }
+
+    disable() {
+        DOM.deleteTheme(this.id);
+    }
+
+}
 
 export default class extends ContentManager {
 
@@ -22,6 +59,17 @@ export default class extends ContentManager {
 
     static get loadAllThemes() {
         return this.loadAllContent;
+    }
+
+    static get loadContent() { return this.loadTheme }
+    static async loadTheme(paths, configs, info, main) {
+        try {
+            const css = await FileUtils.readFile(paths.mainPath);
+            const instance = new Theme({ configs, info, main, paths: { contentPath: paths.contentPath, dirName: paths.dirName }, css });
+            return instance;
+        } catch (err) {
+            throw err;
+        }
     }
 
 }

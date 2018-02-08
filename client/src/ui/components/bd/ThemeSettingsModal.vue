08b791e547a4952dc738ac12ac2f9de5efa70f00
Samuel Elliott <samuelthomas2774@users.noreply.github.com>
2018-02-08 20:19:26 +0000

    Add theme configuration and saving

---
+++ client/src/ui/components/bd/ThemeSettingsModal.vue
@@ -0,0 +1,134 @@
+/**
+ * BetterDiscord Theme Settings Modal Component
+ * Copyright (c) 2015-present Jiiks/JsSucks - https://github.com/Jiiks / https://github.com/JsSucks
+ * All rights reserved.
+ * https://betterdiscord.net
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+*/
+
+<template>
+    <div class="bd-plugin-settings-modal" :class="{'bd-edited': changed}">
+        <div class="bd-backdrop" @click="attemptToClose" :class="{'bd-backdrop-out': closing}"></div>
+        <Modal :headerText="theme.name + ' Settings'" :close="attemptToClose" :class="{'bd-modal-out': closing}">
+            <div slot="body" class="bd-plugin-settings-body">
+                <template v-for="category in configCache">
+                    <div v-if="category.category === 'default' || !category.type">
+                        <PluginSetting v-for="setting in category.settings" :key="setting.id" :setting="setting" :change="settingChange" :changed="setting.changed"/>
+                    </div>
+                    <div v-else-if="category.type === 'static'">
+                        <div class="bd-form-header">
+                            <span class="bd-form-header-text">{{category.category}} static with header</span>
+                        </div>
+                        <PluginSetting v-for="setting in category.settings" :key="setting.id" :setting="setting" :change="settingChange" :changed="setting.changed"/>
+                    </div>
+                    <Drawer v-else-if="category.type === 'drawer'" :label="category.category + ' drawer'">
+                        <PluginSetting v-for="setting in category.settings" :key="setting.id" :setting="setting" :change="settingChange" :changed="setting.changed"/>
+                    </Drawer>
+                    <div v-else>
+                        <PluginSetting v-for="setting in category.settings" :key="setting.id" :setting="setting" :change="settingChange" :changed="setting.changed"/>
+                    </div>
+                </template>
+
+            </div>
+            <div slot="footer" class="bd-footer-alert" :class ="{'bd-active': changed, 'bd-warn': warnclose}">
+                <div class="bd-footer-alert-text">Unsaved changes</div>
+                <div class="bd-button bd-reset-button bd-tp" :class="{'bd-disabled': saving}" @click="resetSettings">Reset</div>
+                <div class="bd-button bd-ok" :class="{'bd-disabled': saving}" @click="saveSettings">
+                    <div v-if="saving" class="bd-spinner-7"></div>
+                    <template v-else>Save Changes</template>
+                </div>
+            </div>
+        </Modal>
+    </div>
+</template>
+<script>
+    // Imports
+    import { Modal } from '../common';
+    import PluginSetting from './pluginsetting/PluginSetting.vue';
+    import Drawer from '../common/Drawer.vue';
+
+    export default {
+        props: ['theme', 'close'],
+        data() {
+            return {
+                changed: false,
+                warnclose: false,
+                configCache: [],
+                closing: false,
+                saving: false
+            }
+        },
+        components: {
+            Modal,
+            PluginSetting,
+            Drawer
+        },
+        methods: {
+            checkForChanges() {
+                let changed = false;
+                for (let category of this.configCache) {
+                    const cat = this.theme.themeConfig.find(c => c.category === category.category);
+                    for (let setting of category.settings) {
+                        if (cat.settings.find(s => s.id === setting.id).value !== setting.value) {
+                            changed = true;
+                            setting.changed = true;
+                        } else {
+                            setting.changed = false;
+                        }
+                    }
+                }
+                return changed;
+            },
+            settingChange(settingId, newValue) {
+                for (let category of this.configCache) {
+                    const found = category.settings.find(s => s.id === settingId);
+                    if (found) {
+                        found.value = newValue;
+                        break;
+                    }
+                }
+                this.changed = this.checkForChanges();
+                this.$forceUpdate();
+            },
+            async saveSettings() {
+                if (this.saving) return;
+                this.saving = true;
+                try {
+                    await this.theme.saveSettings(this.configCache);
+                    this.configCache = JSON.parse(JSON.stringify(this.theme.themeConfig));
+                    this.changed = false;
+                } catch (err) {
+                    // TODO Display error that settings failed to save
+                    console.log(err);
+                }
+                this.saving = false;
+            },
+            resetSettings() {
+                if (this.saving) return;
+                this.configCache = JSON.parse(JSON.stringify(this.theme.themeConfig));
+                this.changed = false;
+                this.$forceUpdate();
+            },
+            attemptToClose(e) {
+                if (!this.changed) {
+                    this.closing = true;
+                    setTimeout(() => {
+                        this.close();
+                    }, 200);
+                    return;
+                }
+                this.warnclose = true;
+                setTimeout(() => {
+                    this.warnclose = false;
+                }, 400);
+            }
+        },
+        beforeMount() {
+            this.configCache = JSON.parse(JSON.stringify(this.theme.themeConfig));
+            console.log(this.configCache);
+            this.changed = this.checkForChanges();
+        }
+    }
+</script>

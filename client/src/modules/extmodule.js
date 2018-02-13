44832fb11a6646db95d794003cd3a2f4df7abbf0
Jiiks <jiiks@windowslive.com>
2018-02-14 00:15:46 +0200

    Exports

--- client/src/modules/extmodule.js
+++ client/src/modules/extmodule.js
@@ -8,7 +8,7 @@
  * LICENSE file in the root directory of this source tree.
 */
 
-export class ExtModule {
+export default class ExtModule {
 
     constructor(pluginInternals) {
         this.__pluginInternals = pluginInternals;

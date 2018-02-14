eb9e3dd4c602fc049dda48d45c20b9034c64e54e
Jiiks <jiiks@windowslive.com>
2018-02-14 22:15:27 +0200

    Updater runs every 30 minutes

---
+++ client/src/modules/updater.js
@@ -0,0 +1,41 @@
+/**
+ * BetterDiscord Updater Module
+ * Copyright (c) 2015-present Jiiks/JsSucks - https://github.com/Jiiks / https://github.com/JsSucks
+ * All rights reserved.
+ * https://betterdiscord.net
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+*/
+
+import Events from './events';
+import Globals from './globals';
+import { $ } from 'vendor';
+import { ClientLogger as Logger } from 'common';
+
+export default class {
+
+    constructor() {
+        window.updater = this;
+        this.init = this.init.bind(this);
+        this.checkForUpdates = this.checkForUpdates.bind(this);
+    }
+
+    get interval() {
+        return 60 * 1000 * 30;
+    }
+
+    init() {
+        setInterval(this.checkForUpdates, this.interval);
+    }
+
+    checkForUpdates() {
+        Events.emit('update-check-start');
+        Logger.info('Updater', 'Checking for updates');
+        $.get('https://raw.githubusercontent.com/JsSucks/BetterDiscordApp/master/package.json', e => {
+            const parse = JSON.parse(e);
+            Logger.info('Updater', `Latest Version: ${parse.version} - Current Version: ${Globals.getObject('version')}`);
+        });
+    }
+
+}

26404843c2036351b84964974c3c8d1f648dc237
Samuel Elliott <samuelthomas2774@users.noreply.github.com>
2018-03-05 19:48:39 +0000

    Fix issues with multiline text settings

--- client/src/ui/components/bd/setting/Multiline.vue
+++ client/src/ui/components/bd/setting/Multiline.vue
@@ -16,18 +16,21 @@
             </div>
             <div class="bd-hint">{{ setting.hint }}</div>
         </div>
-        <div class="bd-form-textarea-wrap">
-            <div contenteditable="true" @keyup.stop @input="input">{{ setting.value }}</div>
-        </div>
+        <textarea class="bd-textarea" ref="textarea" @keyup.stop @input="recalculateHeight" v-model="setting.value" :disabled="setting.disabled"></textarea>
     </div>
 </template>
 <script>
     export default {
-        props: ['setting', 'change'],
+        props: ['setting'],
         methods: {
-            input(e) {
-                this.change(e.target.textContent);
+            recalculateHeight() {
+                const { textarea } = this.$refs;
+                textarea.style.height = '1px';
+                textarea.style.height = textarea.scrollHeight + 2 + 'px';
             }
+        },
+        mounted() {
+            this.recalculateHeight();
         }
     }
 </script>

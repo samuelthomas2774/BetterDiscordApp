5d4548aecae89a88e90749ab9b14167b3372bea3
Samuel Elliott <samuelthomas2774@users.noreply.github.com>
2018-03-18 16:44:43 +0000

    Fix typo and use electron.shell to open links

--- installer/src/ui/intro.vue
+++ installer/src/ui/intro.vue
@@ -3,11 +3,20 @@
         <h3>Welcome to the BetterDiscord setup</h3>
         <div className="separator"></div>
         <span>The setup will install BetterDiscord on your computer.</span>
-        <span>Click "Next" to continue or "Cancel" to exit the stup.</span>
-        <span>For support join the official BetterDiscord <a href="https://v2.betterdiscord.net/discord/support" target="_blank">#support</a> channel in Discord</span>
+        <span>Click "Next" to continue or "Cancel" to exit the setup.</span>
+        <span>For support join the official BetterDiscord <a href="https://v2.betterdiscord.net/discord/support" @click="openLink">#support</a> channel in Discord</span>
     </div>
 </template>
 
 <script>
-    export default {}
+    import electron from 'electron';
+
+    export default {
+        methods: {
+            openLink(e) {
+                electron.shell.openExternal(e.target.href);
+                e.preventDefault();
+            }
+        }
+    }
 </script>

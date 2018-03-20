1772edd37c44180c4affdfc7206a2daaeff9f090
Samuel Elliott <samuelthomas2774@users.noreply.github.com>
2018-03-20 23:29:00 +0000

    Cleanup project root and update package-lock.json

--- gulpfile.js
+++ gulpfile.js
@@ -18,52 +18,52 @@ const client = function() {
         rename(`client.${clientpkg.version}.js`),
         gulp.dest('./release')
     ]);
-}
+};
 
 const core = function() {
-    return pump([
-        gulp.src('./core/dist/modules/**/*'),
-        copy('release/', { prefix: 2 })
-    ]);
-}
-
-const core2 = function() {
     return pump([
         gulp.src('./core/dist/main.js'),
         rename(`core.${corepkg.version}.js`),
         gulp.dest('./release')
     ]);
-}
-
-const core3 = function() {
-    return fs.writeFileSync('./release/index.js', `module.exports = require('./core.${corepkg.version}.js');`);
-}
+};
 
 const sparkplug = function() {
     return pump([
         gulp.src('./core/dist/sparkplug.js'),
         gulp.dest('./release')
     ]);
-}
+};
+
+const core_modules = function() {
+    return pump([
+        gulp.src('./core/dist/modules/**/*'),
+        copy('release/', { prefix: 2 })
+    ]);
+};
+
+const index = function() {
+    return fs.writeFileSync('./release/index.js', `module.exports = require('./core.${corepkg.version}.js');`);
+};
 
 const cssEditor = function() {
     return pump([
         gulp.src('./csseditor/dist/**/*'),
         copy('release/csseditor', { prefix: 2 })
     ]);
-}
+};
 
 const deps = function() {
     return copydeps('./', './release');
-}
+};
 
-const bindings = function() {
+const node_sass_bindings = function() {
     return pump([
         gulp.src('./other/node_sass_bindings/**/*'),
         copy('release/node_modules/node-sass/vendor', { prefix: 2 })
     ]);
-}
+};
 
 gulp.task('release', function () {
-    del(['./release/**/*']).then(() => merge(client(), core(), core2(), core3(), sparkplug(), cssEditor(), deps(), bindings()));
+    del(['./release/**/*']).then(() => merge(client(), core(), sparkplug(), core_modules(), index(), cssEditor(), deps(), node_sass_bindings()));
 });

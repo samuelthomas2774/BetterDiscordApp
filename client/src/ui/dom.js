906140686f6314e7a641feb82661cce1cf3017a4
Samuel Elliott <samuel@fancy.org.uk>
2018-06-10 22:51:52 +0100

    Fix Utils is not defined

--- client/src/ui/dom.js
+++ client/src/ui/dom.js
@@ -8,7 +8,7 @@
  * LICENSE file in the root directory of this source tree.
 */
 
-import { ClientLogger as Logger } from 'common';
+import { Utils, ClientLogger as Logger } from 'common';
 
 class BdNode {
     constructor(tag, className, id) {

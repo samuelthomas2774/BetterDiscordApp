eae1bdf3a6ff5a541709d909f103aa6bcc27e183
Samuel Elliott <samuel@fancy.org.uk>
2018-06-26 23:41:39 +0100

    Fix Utils is not defined

--- client/src/modules/webpackmodules.js
+++ client/src/modules/webpackmodules.js
@@ -8,7 +8,7 @@
  * LICENSE file in the root directory of this source tree.
 */
 
-import { Filters } from 'common';
+import { Utils, Filters } from 'common';
 import Events from './events';
 
 const KnownModules = {

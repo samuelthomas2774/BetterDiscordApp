e660ce6a31580d4ed0d6919936bce1f5e856678c
Zack Rauen <rauenzi@outlook.com>
2018-08-08 23:39:57 -0400

    remove redundant array check, fix strings

--- client/src/ui/classnormaliser.js
+++ client/src/ui/classnormaliser.js
@@ -26,15 +26,14 @@ export default class ClassNormaliser extends Module {
 
     shouldIgnore(value) {
         if (!isNaN(value)) return true;
-        if (value.endsWith("px") || value.endsWith("ch") || value.endsWith("em") || value.endsWith("ms")) return true;
-        if (value.startsWith("rgba")) return true;
-        if (value.includes("calc(")) return true;
+        if (value.endsWith('px') || value.endsWith('ch') || value.endsWith('em') || value.endsWith('ms')) return true;
+        if (value.startsWith('rgba')) return true;
+        if (value.includes('calc(')) return true;
         return false;
     }
 
     moduleFilter(module) {
         if (typeof module !== 'object' || Array.isArray(module)) return false;
-        if (Array.isArray(module)) return false;
         if (module.__esModule) return false;
         if (!Object.keys(module).length) return false;
         for (let baseClassName in module) {
@@ -52,11 +51,11 @@ export default class ClassNormaliser extends Module {
         for (let baseClassName in classNames) {
             const value = classNames[baseClassName];
             if (this.shouldIgnore(value)) continue;
-            const classList = value.split(" ");
+            const classList = value.split(' ');
             for (let normalClass of classList) {
                 const match = normalClass.match(randClass)[1];
                 if (!match) continue; // Shouldn't ever happen since they passed the moduleFilter, but you never know
-                const camelCase = match.split('-').map((s, i) => i ? s[0].toUpperCase() + s.slice(1) : s).join("");
+                const camelCase = match.split('-').map((s, i) => i ? s[0].toUpperCase() + s.slice(1) : s).join('');
                 classNames[baseClassName] += ` ${componentName}-${camelCase}`;
             }
         }

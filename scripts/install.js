43ec860bfe9ef6f7683af298323bca7168e2e384
Samuel Elliott <samuel@fancy.org.uk>
2018-08-23 18:28:01 +0100

    Add keytar and bindings

--- scripts/install.js
+++ scripts/install.js
@@ -16,3 +16,26 @@ for (const node_sass_binding_name of fs.readdirSync(prebuilt_node_sass_bindings_
 
     fs.copySync(binding_path, installation_path);
 }
+
+// Copy the keytar bindings to node_modules/keytar/vendor
+const keytar_path = path.resolve(require.resolve('keytar'), '..', '..');
+const prebuilt_keytar_bindings_path = path.resolve(__dirname, '..', 'other', 'keytar', 'keytar.node');
+const keytar_release_path = path.join(keytar_path, 'build', 'Release');
+const keytar_binding_path = path.join(keytar_release_path, 'keytar.node');
+
+if (fs.existsSync(path.join(keytar_release_path, 'keytar.node'))) {
+    const stat = fs.statSync(path.join(keytar_release_path, 'keytar.node'));
+
+    // if (stat.isFile()) {
+        console.log('Deleting keytar binding');
+        fs.removeSync(path.join(keytar_path, 'build'));
+    // }
+}
+
+if (!fs.existsSync(path.join(keytar_release_path, 'keytar.node'))) {
+    fs.mkdirpSync(keytar_release_path);
+
+    console.log('Copying keytar bindings from', prebuilt_keytar_bindings_path, 'to', keytar_binding_path);
+
+    fs.copySync(prebuilt_keytar_bindings_path, keytar_binding_path);
+}

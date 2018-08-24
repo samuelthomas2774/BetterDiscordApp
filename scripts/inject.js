49b80d5078da599c0d0ce402d01e99f1d3b226b7
Samuel Elliott <samuel@fancy.org.uk>
2018-08-24 11:27:17 +0100

    Fix inject script for macOS and Linux and allow passing the full path to Discord.app on macOS

--- scripts/inject.js
+++ scripts/inject.js
@@ -3,26 +3,27 @@ const fs = require('fs');
 const path = require('path');
 
 const releaseInput = args[2] ? args[2].toLowerCase() : '';
-const release = releaseInput == 'canary' ? 'DiscordCanary' : releaseInput == 'ptb' ? 'DiscordPTB' : 'Discord';
+const release = releaseInput === 'canary' ? 'DiscordCanary' : releaseInput === 'ptb' ? 'DiscordPTB' : 'Discord';
 console.log(`Injecting into version ${release}`);
+
 const discordPath = (function() {
     if (process.platform === 'win32') {
         const basedir = path.join(process.env.LOCALAPPDATA, release);
         if (!fs.existsSync(basedir)) throw new Error(`Cannot find directory for ${release}`);
         const version = fs.readdirSync(basedir).filter(f => fs.lstatSync(path.join(basedir, f)).isDirectory() && f.split('.').length > 1).sort().reverse()[0];
-        const dir = path.join(basedir, version, 'resources');
-        if (!version || !fs.existsSync(dir)) throw new Error(`Cannot find directory for ${release}`);
-        return dir;
+        return path.join(basedir, version, 'resources');
+    } else if (process.platform === 'darwin') {
+        const appPath = releaseInput === 'canary' ? path.join('/Applications', 'Discord Canary.app')
+            : releaseInput === 'ptb' ? path.join('/Applications', 'Discord PTB.app')
+            : args[2] ? args[2] : path.join('/Applications', 'Discord.app');
+
+        return path.join(appPath, 'Contents', 'Resources');
+    } else if (process.platform === 'linux') {
+        return path.join('/usr', 'share', release.toLowerCase(), 'resources');
     }
-    else if (process.platform == 'darwin') {
-        const dir = path.join('Applications', `${release}.app`, 'Contents', 'Resources');
-        if (!fs.existsSync(dir)) throw new Error(`Cannot find directory for ${release}`);
-        return dir;
-    }
-    const dir = path.join('usr', 'share', release.toLowerCase(), 'resources');
-    if (!fs.existsSync(dir)) throw new Error(`Cannot find directory for ${release}`);
-    return dir;
 })();
+
+if (!fs.existsSync(discordPath)) throw new Error(`Cannot find directory for ${release}`);
 console.log(`Found ${release} in ${discordPath}`);
 
 const appPath = path.join(discordPath, 'app');
@@ -38,8 +39,7 @@ fs.writeFileSync(packageJson, JSON.stringify({
     name: 'betterdiscord',
     description: 'BetterDiscord',
     main: 'index.js',
-    private: true,
-    dependencies: {}
+    private: true
 }, null, 4));
 
 console.log(`Writing index.js`);
@@ -48,9 +48,9 @@ const fs = require('fs');
 const Module = require('module');
 const electron = require('electron');
 const basePath = path.join(__dirname, '..', 'app.asar');
-const pkg = require(path.join(basePath, 'package.json'));
 electron.app.getAppPath = () => basePath;
-Module._load(path.join(basePath, pkg.main), null, true);
-electron.app.on('ready', () => {new (require('${path.join(__dirname, '..', 'core').replace(/\\/g, '/')}').BetterDiscord)();});`);
+Module._load(basePath, null, true);
+electron.app.on('ready', () => new (require('${path.join(__dirname, '..', 'core').replace(/\\/g, '\\\\').replace(/'/g, '\\\'')}').BetterDiscord)());
+`);
 
 console.log(`Injection successful, please restart ${release}.`);

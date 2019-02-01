27df24f369536bda364726951877bebeb86ce80e
Jiiks <jiiks@windowslive.com>
2019-02-01 15:00:03 +0200

    Update stub

--- installer/stub.js
+++ installer/stub.js
@@ -1,4 +1,4 @@
-const { paths } = require('./bd.json');
+const bdinfo = require('./bd.json');
 const { app } = require('electron');
 const path = require('path');
 const fs = require('fs');
@@ -8,8 +8,9 @@ const packagePath = path.join(__dirname, '..', 'app.asar');
 app.getAppPath = () => packagePath;
 
 function loadBd() {
+    const { paths } = bdinfo;
     const { BetterDiscord } = require(paths.core);
-    const instance = new BetterDiscord();
+    const instance = new BetterDiscord(bdinfo);
 }
 
 app.on('ready', loadBd);

5408d994be52f3fb2e771f3514c334decd0ec764
Jiiks <jiiks@windowslive.com>
2019-03-04 09:52:25 +0200

    babel config for client

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,10 +6,7 @@ const jsLoader = {
     test: /\.(js|jsx)$/,
     exclude: /node_modules/,
     use: {
-        loader: 'babel-loader',
-        options: {
-            presets: ['@babel/env', '@babel/react']
-        }
+        loader: 'babel-loader'
     }
 };
 

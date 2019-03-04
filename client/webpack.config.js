92845728cc1741351368832f548232bdbc5c5278
Jiiks <jiiks@windowslive.com>
2019-03-04 10:09:58 +0200

    command line pls

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,69 +1,19 @@
 const path = require('path');
+
+const baseconfig = require('./webpack.base.config');
+
+const merge = require('webpack-merge');
 const webpack = require('webpack');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
-const jsLoader = {
-    test: /\.(js|jsx)$/,
-    exclude: /node_modules/,
-    use: {
-        loader: 'babel-loader'
-    }
-};
-
-const vueLoader = {
-    test: /\.(vue)$/,
-    use: 'vue-loader'
-};
-
-const scssLoader = {
-    test: /\.scss$/,
-    exclude: /node_modules/,
-    use: ['css-loader', 'sass-loader']
-};
-
-module.exports = {
-    entry: './src/index.js',
+const config = {
     mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'betterdiscord.client.js'
     },
-    module: {
-        rules: [jsLoader, vueLoader, scssLoader]
-    },
-    externals: {
-        electron: 'require("electron")',
-        asar: 'require("asar")',
-        fs: 'require("fs")',
-        path: 'require("path")',
-        util: 'require("util")',
-        process: 'require("process")',
-        net: 'require("net")',
-        request: 'require(require("path").join(require("electron").remote.app.getAppPath(), "node_modules", "request"))',
-        sparkplug: 'require("../../core/dist/sparkplug")',
-        'node-crypto': 'require("crypto")'
-    },
-    resolve: {
-        alias: {
-            vue$: path.resolve('..', 'node_modules', 'vue', 'dist', 'vue.esm.js')
-        },
-        modules: [
-            path.resolve('..', 'node_modules'),
-            path.resolve('..', 'common', 'modules'),
-            path.resolve('src', 'modules'),
-            path.resolve('src', 'ui'),
-            path.resolve('src', 'plugins'),
-            path.resolve('src', 'structs'),
-            path.resolve('src', 'builtin')
-        ]
-    },
-    node: {
-        process: false,
-        __filename: false,
-        __dirname: false
-    },
     plugins: [
-        new webpack.NamedModulesPlugin(),
-        new VueLoaderPlugin()
+        new webpack.NamedModulesPlugin()
     ]
 };
+
+module.exports = merge(baseconfig, config);
--- client/webpack.production.config.js
+++ client/webpack.production.config.js
@@ -1,74 +1,21 @@
 const path = require('path');
+
+const baseconfig = require('./webpack.base.config');
+
+const merge = require('webpack-merge');
 const webpack = require('webpack');
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
-const jsLoader = {
-    test: /\.(js|jsx)$/,
-    exclude: /node_modules/,
-    use: {
-        loader: 'babel-loader',
-        options: {
-            presets: ['@babel/env', '@babel/react']
-        }
-    }
-};
-
-const vueLoader = {
-    test: /\.(vue)$/,
-    use: 'vue-loader'
-};
-
-const scssLoader = {
-    test: /\.scss$/,
-    exclude: /node_modules/,
-    use: ['css-loader', 'sass-loader']
-};
-
-module.exports = {
-    entry: './src/index.js',
+const config = {
     mode: 'production',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'betterdiscord.client-release.js'
     },
-    module: {
-        rules: [jsLoader, vueLoader, scssLoader]
-    },
-    externals: {
-        electron: 'require("electron")',
-        fs: 'require("fs")',
-        path: 'require("path")',
-        util: 'require("util")',
-        process: 'require("process")',
-        net: 'require("net")',
-        request: 'require(require("path").join(require("electron").remote.app.getAppPath(), "node_modules", "request"))',
-        sparkplug: 'require("./sparkplug")',
-        'node-crypto': 'require("crypto")',
-        'child_process': 'require("child_process")'
-    },
-    resolve: {
-        alias: {
-            vue$: path.resolve('..', 'node_modules', 'vue', 'dist', 'vue.esm.js')
-        },
-        modules: [
-            path.resolve('..', 'node_modules'),
-            path.resolve('..', 'common', 'modules'),
-            path.resolve('src', 'modules'),
-            path.resolve('src', 'ui'),
-            path.resolve('src', 'plugins'),
-            path.resolve('src', 'structs'),
-            path.resolve('src', 'builtin')
-        ]
-    },
-    node: {
-        process: false,
-        __filename: false,
-        __dirname: false
-    },
     plugins: [
         new webpack.DefinePlugin({
             PRODUCTION: JSON.stringify(true)
-        }),
-        new VueLoaderPlugin()
+        })
     ]
 };
+
+module.exports = merge(baseconfig, config);

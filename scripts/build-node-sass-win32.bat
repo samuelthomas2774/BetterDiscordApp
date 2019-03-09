436f3d3c360b7407e95e5caae485bb9a022d015e
Samuel Elliott <samuel@fancy.org.uk>
2019-03-09 20:41:40 +0000

    macOS keytar binding

--- scripts/build-node-sass-win32.bat
+++ scripts/build-node-sass-win32.bat
@@ -1,13 +1,7 @@
 set "PLATFORM=win32"
 
-set "NODE_API_VERSION=53"
-set "ELECTRON_VERSION=1.6.15"
-
-call ./scripts/build-node-sass.bat %PLATFORM% ia32 %NODE_API_VERSION% %ELECTRON_VERSION%
-call ./scripts/build-node-sass.bat %PLATFORM% x64 %NODE_API_VERSION% %ELECTRON_VERSION%
-
 set "NODE_API_VERSION=64"
-set "ELECTRON_VERSION=4.0.0-beta.7"
+set "ELECTRON_VERSION=4.0.0-beta.11"
 
 call ./scripts/build-node-sass.bat %PLATFORM% ia32 %NODE_API_VERSION% %ELECTRON_VERSION%
 call ./scripts/build-node-sass.bat %PLATFORM% x64 %NODE_API_VERSION% %ELECTRON_VERSION%

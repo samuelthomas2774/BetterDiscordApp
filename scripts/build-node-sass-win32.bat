5638f8cfb0c7c05efc548423e6c5e8f610e0cacc
Samuel Elliott <samuel@fancy.org.uk>
2019-04-06 03:14:09 +0100

    Remove old bindings for Electron 4.0.0-beta.11

--- scripts/build-node-sass-win32.bat
+++ scripts/build-node-sass-win32.bat
@@ -1,11 +1,5 @@
 set "PLATFORM=win32"
 
-set "NODE_API_VERSION=64"
-set "ELECTRON_VERSION=4.0.0-beta.11"
-
-call ./scripts/build-node-sass.bat %PLATFORM% ia32 %NODE_API_VERSION% %ELECTRON_VERSION%
-call ./scripts/build-node-sass.bat %PLATFORM% x64 %NODE_API_VERSION% %ELECTRON_VERSION%
-
 set "NODE_API_VERSION=69"
 set "ELECTRON_VERSION=4.0.8"
 
